b11fb1fec41fdcf7544251a3c327748ee7c26793
Yong Cheng Low <lowyongcheng@hotmail.com>
2023-08-19 11:14:20 +0800

    scaffold article

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
+gem "simple_form"
+gem "sassc-rails" # Uncomment this line
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    root to: "articles#index"
+    resources :articles, except: :index
+  end
 end
